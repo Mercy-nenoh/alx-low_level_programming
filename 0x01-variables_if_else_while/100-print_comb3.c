a863c4c1bf8f7f82feaa5a1d7ad9de620225d486
Mercy-nenoh <mercynenoh94@gmail.com>
2022-06-06 10:00:10 -0700

    Tasak 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,16 +15,16 @@ int main(void)
 		for (ones = tens + 1; ones <= 9; ones++)
 		{
 			putchar(tens + '0');
-			 putchar(ones + '0');
+			putchar(ones + '0');
 
-			  if (tens < 8) 
-			  {
-				  putchar(',');
-				   putchar(' ');
-			  }
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-	 putchar('\n');
+	putchar('\n');
 
- return (0);
+return (0);
 }
