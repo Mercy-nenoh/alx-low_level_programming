4118a6ca86fa8f2e4ab84ea8394cb22856173fc0
Mercy-nenoh <mercynenoh94@gmail.com>
2022-06-13 09:56:52 -0700

    Pointers,arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,18 @@
 /**
  * _puts - Prints a string.
  * @str: string.
+ * Return: void
  */
+
 void _puts(char *str)
 {
-	while (*str)
-		putchar(*str++);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
 	_putchar('\n');
 }
