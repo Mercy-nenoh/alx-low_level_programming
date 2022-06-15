0598d5002de0b273c89f139047d127872e3fef7d
Mercy-nenoh <mercynenoh94@gmail.com>
2022-06-15 11:16:13 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _strcmp - compares two strings
- * @s1: First string
- * @s2: Second string
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ * @s1: Input value
+ * @s2: Input value
+ *
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2);
 {
