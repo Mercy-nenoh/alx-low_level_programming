3a653deac6970e59e660d074ea1bd3cc2c27324f
Mercy-nenoh <mercynenoh94@gmail.com>
2022-06-15 11:35:31 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,16 @@
  * @s: input string
  * Return: the value of the pointer.
  */
-
 char *string_toupper(char *s);
 {
-	int i = 0;
+	int i;
 
-	while (*(s + i) != '\0')
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if ((*(s + i) >= 97) && (*(s + i) <= 122))
-			*(s + i) = *(s + i) - 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		i++;
 	}
-
 	return (s);
 }
