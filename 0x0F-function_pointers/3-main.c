9938ffd9b36326c23b5ed7b20b52b41bd4efbdd5
Mercy-nenoh <mercynenoh94@gmail.com>
2022-07-07 01:54:15 -0700

    Function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,20 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+	int num1, num2;
+	char *oper;
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+		return (98);
 	}
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
-		printf("Error\n"), exit(99);
+		printf("Error\n");
+		return (99);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	printf("%i\n", fptr(a, b));
-	return (1);
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') &&
+			((atoi(argv[3])) == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	oper = argv[2];
+
+	printf("%d\n", get_op_func(oper)(num1, num2));
+	return (0);
 }
