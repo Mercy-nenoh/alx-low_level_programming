4192b9bd004b9aaf8c769d85ce32609a81b5f31e
Mercy-nenoh <mercynenoh94@gmail.com>
2022-07-14 23:12:24 -0700

    Singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
  * Return: address of the head.
  */
 
-st_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str);
 {
 	list_t *new;
 	unsigned int len = 0;
@@ -27,4 +27,3 @@ st_t *add_node(list_t **head, const char *str)
 
 	return (*head);
 }
-
